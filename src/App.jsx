94de6ae2a03ff1fd9e6b42a0fe01fa9e195357c6
Erubey Cazares <aecazares@hotmail.com>
2024-07-20 17:46:39 -0600

    useSearch custom hook for handling the input query and error

--- src/App.jsx
+++ src/App.jsx
@@ -2,18 +2,37 @@ import React from 'react'
 import './App.css'
 import { Movies } from './components/Movies'
 import { useMovies } from './hooks/useMovies'
+import { useSearch } from './hooks/useSearch'
 
 const App = () => {
-  const { movies } = useMovies()
+  const { movies } = useMovies();
+  const { query, setQuery, error } = useSearch();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log({ query });
+  }
+
+  const handleInputChange = (e) => {
+    const newQuery = e.target.value;
+    if (newQuery.startsWith(' ')) return
+    setQuery(newQuery);
+  }
 
   return (
     <div className='page'>
       <header>
         <h1 style={{ textAlign: 'center' }}>Movie Finder</h1>
-        <form action="" className='form'>
-          <input type="text" placeholder="The Lord Of The Rings, Star Wars, The Godfather..." />
-          <button type="submit" >Search</button>
+        <form className='form' onSubmit={handleSubmit}>
+          <input
+            value={query}
+            onChange={handleInputChange}
+            style={error ? { border: '1px solid red' } : null}
+            placeholder="The Lord Of The Rings, Star Wars, The Godfather..."
+          />
+          <button type="submit">Search</button>
         </form>
+        {error && <h4 style={{ color: 'red', textAlign: 'center' }} >{error}</h4>}
       </header>
 
       <main>
--- src/components/Movies.jsx
+++ src/components/Movies.jsx
@@ -1,3 +1,5 @@
+import React from "react"
+
 const MoviesList = ({ movies }) => {
 	return (
 		<ul>
